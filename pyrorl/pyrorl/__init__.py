fbaaa59601dc0f95df15a3b300657c0835e17e00
aliakarma <443059463@stu.iu.edu.sa>
2026-04-30 22:58:00 +0300

    Update __init__.py

--- pyrorl/pyrorl/__init__.py
+++ pyrorl/pyrorl/__init__.py
@@ -2,6 +2,6 @@ from gymnasium import register
 
 register(
     id="pyrorl/PyroRL-v0",
-    entry_point="pyrorl.envs:WildfireEvacuationEnv",
+    entry_point="pyrorl.pyrorl.envs:WildfireEvacuationEnv",
     max_episode_steps=200,
 )
